6f2f37509a9e499f08d91f63fa84b1aba9564f5f
Paulino Auyon <pauyon@jgboswell.com>
2025-06-24 13:12:54 -0700

    Add update method

--- src/http.js
+++ src/http.js
@@ -8,3 +8,21 @@ export async function fetchAvailablePlaces() {
 
     return resData.places;
 }
+
+export async function updateUserPlaces(places) {
+    fetch('http://localhost:3000/user-places', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ places }),
+    });
+
+    const resData = await response.json();
+
+    if (!response.ok) {
+        throw new Error(resData.message || 'Could not update user places.');
+    }
+
+    return resData.message;
+}
