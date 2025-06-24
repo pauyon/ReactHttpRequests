6a2a0d27bdbbd57e68f5376142220251661fccf2
Paulino Auyon <pauyon@jgboswell.com>
2025-06-24 10:56:52 -0700

    Add IsLoading functionality

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -3,13 +3,16 @@ import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isLoading, setIsLoading] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
     async function fetchPlaces() {
+      setIsLoading(true);
       const response = await fetch('http://localhost:3000/places');
       const resData = await response.json();
       setAvailablePlaces(resData.places);
+      setisLoading(false);
     }
 
     fetchPlaces();
@@ -28,6 +31,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
       <Places
         title="Available Places"
         places={availablePlaces}
+        isLoading={isLoading}
+        loadingText="Loading places..."
         fallbackText="No places available."
         onSelectPlace={onSelectPlace}
       />
